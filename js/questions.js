140018a85a0946f4b4d47c004cf7a99122b1a727
RuanyLopes <ruanyllopes@gmail.com>
2024-09-24 22:18:30 -0300

    basic changes

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,6 @@
 
 const questions = [
-    {questao: "Qual dessas opções NÃO é uma linguagem de programação? ", opçoes:["HTML", "Javascript","Python","C#"], resposta:0},
-    {questao: "Qual desses é a nomenclatura de 'html'?", opçoes:["Hypertext Makeup Language","Hypertext Training Mode Language","HyperText Markup Language", "HyperText Madeup Language"], resposta:2},
-    {questao:"Quais as ccaracterísticas a seguir são as características de 'let'?", opçoes:["Pode ser declarado e atualizado","Pode ser redefinido, mas não pode haver outra declaração", "Sempre usado em valores imutáveis", "Sempre usado em valores imutáveis e não pode haver outra declaração"], resposta:1}
+    {questao: "Qual dessas opções NÃO é uma linguagem de programação? ", opcoes:["HTML", "Javascript","Python","C#"], resposta:0},
+    {questao: "Qual desses é a nomenclatura de 'html'?", opcoes:["Hypertext Makeup Language","Hypertext Training Mode Language","HyperText Markup Language", "HyperText Madeup Language"], resposta:2},
+    {questao:"Quais as ccaracterísticas a seguir são as características de 'let'?", opcoes:["Pode ser declarado e atualizado","Pode ser redefinido, mas não pode haver outra declaração", "Sempre usado em valores imutáveis", "Sempre usado em valores imutáveis e não pode haver outra declaração"], resposta:1}
 ]
