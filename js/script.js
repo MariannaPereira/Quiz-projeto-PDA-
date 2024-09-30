409cc429e5325a33a7c3e659d19240d79a664c53
RuanyLopes <ruanyllopes@gmail.com>
2024-09-30 19:57:23 -0300

    changes CSS

--- js/script.js
+++ js/script.js
@@ -27,18 +27,24 @@ function botaoPrincipal(reiniciar) {
   console.log("botão clicado");
 }
 botaoPrincipal();
+function iniciarQuiz(){
+ let telaQuiz = document.getElementById("telaQuiz");
+ telaQuiz.style.display = "none";
+ if(telaQuiz.style.display==="none"){
+  telaQuiz.style.display="block" //verificar o tipo de displayf
+ }
+}
 
-//Esse é a função onclick (quando clica no botão ele tem uma ação), falta fazer o looping
-const botaoComeca = document.getElementById("start-btn");
-const inicio = document.getElementById("quiz-container");
-const telaQuiz = document.getElementById("telaQuiz");
-//add um evento quando o botão começar é clicado
-botaoComeca.addEventListener("click", function () {
-  //esconde a tela inicial
-  inicio.style.display = "none";
-  //mostra a tela de perguntas
-  telaQuiz.style.display = "block";
-  //carrega a pergunta e as respostas
-  mostrarPergunta();
-});
-
+// //Esse é a função onclick (quando clica no botão ele tem uma ação), falta fazer o looping
+// const botaoComeca = document.getElementById("start-btn");
+// const inicio = document.getElementById("quiz-container");
+// const telaQuiz = document.getElementById("telaQuiz");
+// //add um evento quando o botão começar é clicado
+// botaoComeca.addEventListener("click", function () {
+//   //esconde a tela inicial
+//   //inicio.style.display = "none";
+//   //mostra a tela de perguntas
+//   telaQuiz.style.display = "block";
+//   //carrega a pergunta e as respostas
+//   mostrarPergunta();
+// });
